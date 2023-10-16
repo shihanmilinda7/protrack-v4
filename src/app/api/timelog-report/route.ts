9788713dde27fdf2209e82360caac5b228524d30
shihanmilinda7 <shihanmilinda7@gmail.com>
2023-10-16 14:18:44 +0530

    ddd

--- src/app/api/timelog-report/route.ts
+++ src/app/api/timelog-report/route.ts
@@ -1,6 +1,5 @@
-import { prisma } from "@/db";
-import { Prisma } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { getTimelogAsYearMonthForReport } from "./timelogreport-db-api";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -9,31 +8,11 @@ export async function GET(request: Request) {
   const tmpDate: any = searchParams.get("date");
   const tmpStaffid: any = searchParams.get("staffid");
   try {
-    await prisma.$transaction(async (tx) => {
-      const timelogData = await tx.timelogs.findMany({
-        where: {
-          staffid: parseInt(tmpStaffid),
-          date: {
-            startsWith: tmpDate,
-          },
-        },
-      });
-
-      for (let index = 0; index < timelogData.length; index++) {
-        let element: any = timelogData[index];
-        const rawQuery = Prisma.sql`SELECT p.projectname,pt.taskname,td.* FROM timelogsdetails as td left join projects as p on td.projectid = p.projectid left join projecttasks as pt on td.taskid = pt.taskid where td.timelogid = ${element.timelogid}`;
-        const detailData: any = await tx.$queryRaw(rawQuery);
-        element["taskdetails"] = detailData;
-        // console.log("timelogData", detailData);
-        const rawQuery1 = Prisma.sql`SELECT sum(td.time) as totaltime FROM timelogsdetails as td  where td.timelogid = ${element.timelogid}`;
-        const detailData1: any = await tx.$queryRaw(rawQuery1);
-        element["totalHours"] = detailData1[0]?.totaltime?.toString() ?? "0";
-
-      }
-      // console.log("timelogData", timelogData);
-      res = { message: "SUCCESS", timelogData };
-      return "";
-    });
+    const timelogData = await getTimelogAsYearMonthForReport(
+      tmpDate,
+      parseInt(tmpStaffid)
+    );
+    res = { message: "SUCCESS", timelogData };
   } catch (error) {
     console.error("Error getting timelog data source:", error);
     res = { message: "FAIL" };
